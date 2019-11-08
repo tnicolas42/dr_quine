1a8a7e6b451c8db37b07f5276453be6b7c03b9c9
Joffrey Janiec <jjaniec@student.42.fr>
2019-11-08 17:46:08 +0100

    Grace python jul comment

--- Grace/python/Grace.py
+++ Grace/python/Grace.py
@@ -1,6 +1,6 @@
-s = "f = open('Grace_kid.py', 'w+')%cdef a(s): f.write(s %c (10, 37, 10, 37, 37, 37, 37, 37, 10, 10, 10))  # inside%cf.write('s = %cc%cs%cc%cc' %c (34, s, 34, 10))%ca(s)  # outside%cf.close()%c"
+s = "f = open('Grace_kid.py', 'w+')%cdef a(s): f.write(s %c (10, 37, 10, 37, 37, 37, 37, 37, 10, 10, 10)) # jul%cf.write('s = %cc%cs%cc%cc' %c (34, s, 34, 10))%ca(s)%cf.close()%c"
 f = open('Grace_kid.py', 'w+')
-def a(s): f.write(s % (10, 37, 10, 37, 37, 37, 37, 37, 10, 10, 10))  # inside
+def a(s): f.write(s % (10, 37, 10, 37, 37, 37, 37, 37, 10, 10, 10)) # jul
 f.write('s = %c%s%c%c' % (34, s, 34, 10))
-a(s)  # outside
+a(s)
 f.close()
