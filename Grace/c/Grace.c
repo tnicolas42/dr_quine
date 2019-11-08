009d322f1984079ac1e53695695b3ad46cbf86ae
Joffrey Janiec <jjaniec@student.42.fr>
2019-11-08 17:43:59 +0100

    Grace -

--- Grace/c/Grace.c
+++ Grace/c/Grace.c
@@ -1,13 +1,7 @@
-char *s = "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%cint fd;%cvoid a(char *s) { dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 9, 34, 34, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 9, 10, 10, 10); } // a%cint main()%c{%c%cfd = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 0700);%c%cdprintf(fd, %cchar *s = %cc%%s%cc;%cn%c, 34, s, 34); a(s);  // inside the function%c%cclose(fd);%c%creturn 0;%c}%c";
+#define s "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#define WRITE(fd) dprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, 37, 37, 92, 34, 10, 10)%c#define MAIN int main() { int fd = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 0700); dprintf(fd, %c#define s %cc%%s%cc%cn%c, 34, s, 34); WRITE(fd); close(fd); /* jul */ }%cMAIN%c"
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-int fd;
-void a(char *s) { dprintf(fd, s, 10, 10, 10, 10, 10, 10, 10, 9, 34, 34, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 9, 10, 10, 10); } // a
-int main()
-{
-	fd = open("Grace_kid.c", O_CREAT | O_RDWR, 0700);
-	dprintf(fd, "char *s = %c%s%c;\n", 34, s, 34); a(s);  // inside the function
-	close(fd);
-	return 0;
-}
+#define WRITE(fd) dprintf(fd, s, 10, 10, 10, 10, 34, 34, 34, 37, 37, 92, 34, 10, 10)
+#define MAIN int main() { int fd = open("Grace_kid.c", O_CREAT | O_RDWR, 0700); dprintf(fd, "#define s %c%s%c\n", 34, s, 34); WRITE(fd); close(fd); /* jul */ }
+MAIN
