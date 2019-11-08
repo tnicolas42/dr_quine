7957fa5f060f3331de625d629446b0b7bb2bbbe9
tnicolas <tnicolas@student.42.fr>
2019-11-08 14:38:45 +0100

    add Colleen.py

--- Colleen/python/Colleen.py
+++ Colleen/python/Colleen.py
@@ -1,2 +1,4 @@
-cont = "print('cont = ' + chr(34) + '%s' % (cont) + chr(34) + chr(10) + '%s' % (cont))"
-print('cont = ' + chr(34) + '%s' % (cont) + chr(34) + chr(10) + '%s' % (cont))
+s = "def a(s): print(s %c (37, 10, 37, 37, 37, 37, 10))  # inside%cprint('s = %cc%cs%cc' %c (34, s, 34))%ca(s)  # outside"
+def a(s): print(s % (37, 10, 37, 37, 37, 37, 10))  # inside
+print('s = %c%s%c' % (34, s, 34))
+a(s)  # outside
