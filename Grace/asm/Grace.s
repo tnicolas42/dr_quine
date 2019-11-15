95f8bde6990bff42fc7f0bb804301085d9ef98f5
Joffrey Janiec <jjaniec@student.42.fr>
2019-11-15 17:11:59 +0100

    Grace.s

--- Grace/asm/Grace.s
+++ Grace/asm/Grace.s
@@ -1,31 +1,35 @@
 section .data
-s db "section .data%2$cs db %3$c%1$s%3$c, 0%2$clens equ $ - s%2$cfilename db %3$cGrace_kid.s%3$c, 0%2$cfd dq 0%2$csection .text%2$cglobal _main%2$cextern _dprintf%2$c_main:%2$cpush rbp%2$cmov rax, 0x2000005%2$clea rdi, [rel filename]%2$cmov rsi, 0202h%2$cmov rdx, 00666o%2$csyscall%2$cmov [rel fd], rax%2$cmov rdi, [rel fd]%2$clea rsi, [rel s]  ; arg 1%2$clea rdx, [rel s]  ; arg 2%2$cmov rcx, 10%2$cmov r8, 34%2$ccall _dprintf%2$clea rdi, [rel fd]%2$cmov rax, 0x2000003%2$csyscall%2$ccall usefull%2$cpop rbp%2$cmov rax, 0%2$cret%2$cusefull:%2$cret%2$c", 0
-lens equ $ - s
-filename db "Grace_kid.s", 0
-fd dq 0
+fileName:  db 'Grace_kid.s',0
+fileFlags: dq 0202h; jul
+fileMode:  dq 00666o
+fileDescriptor: dq 0
+s:
+.str db "section .data%2$cfileName:  db 'Grace_kid.s',0%2$cfileFlags: dq 0202h; jul%2$cfileMode:  dq 00666o%2$cfileDescriptor: dq 0%2$cs:%2$c.str db %3$c%1$s%3$c%2$csection .text%2$cglobal _main%2$cextern _dprintf%2$c_main:%2$cpush rbp%2$cmov rax,0x2000005%2$clea rdi,[rel fileName]%2$cmov rsi,[rel fileFlags]%2$cmov rdx,[rel fileMode]%2$csyscall%2$cmov [rel fileDescriptor], rax%2$c%2$cmov rdi,[rel fileDescriptor]%2$clea rsi,[rel s.str]%2$clea rdx,[rel s.str]%2$cmov rcx,10%2$cmov r8,34%2$ccall _dprintf%2$ccall usefull%2$c%2$cmov rax,0x2000003%2$cmov rdi,[rel fileDescriptor]%2$csyscall%2$cpop rbp%2$cmov rax,0%2$cret%2$cusefull:%2$cret%2$c"
 section .text
 global _main
 extern _dprintf
 _main:
 push rbp
-mov rax, 0x2000005
-lea rdi, [rel filename]
-mov rsi, 0202h
-mov rdx, 00666o
+mov rax,0x2000005
+lea rdi,[rel fileName]
+mov rsi,[rel fileFlags]
+mov rdx,[rel fileMode]
 syscall
-mov [rel fd], rax
-mov rdi, [rel fd]
-lea rsi, [rel s]  ; arg 1
-lea rdx, [rel s]  ; arg 2
-mov rcx, 10
-mov r8, 34
+mov [rel fileDescriptor], rax
+
+mov rdi,[rel fileDescriptor]
+lea rsi,[rel s.str]
+lea rdx,[rel s.str]
+mov rcx,10
+mov r8,34
 call _dprintf
-lea rdi, [rel fd]
-mov rax, 0x2000003
-syscall
 call usefull
+
+mov rax,0x2000003
+mov rdi,[rel fileDescriptor]
+syscall
 pop rbp
-mov rax, 0
+mov rax,0
 ret
 usefull:
 ret
