a3f3f03f1a947d017d07d711ea1408587a30e2f4
tnicolas <tnicolas@student.42.fr>
2019-11-15 17:41:58 +0100

    change comment style

--- Colleen/c/Colleen.c
+++ Colleen/c/Colleen.c
@@ -1,6 +1,9 @@
-char *s = "#include <stdio.h>%cvoid a(char *s) { printf(s, 10, 10, 10, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 10, 10); } // a%cint main()%c{%c%cprintf(%cchar *s = %cc%%s%cc;%cn%c, 34, s, 34); a(s);  // inside the function%c%creturn 0;%c}%c";
+char *s = "#include <stdio.h>%c/*%coutside%c*/%cvoid a(char *s) { printf(s, 10, 10, 10, 10, 10, 10, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 10, 10); }%cint main()%c{%c%cprintf(%cchar *s = %cc%%s%cc;%cn%c, 34, s, 34); a(s);  // inside the function%c%creturn 0;%c}%c";
 #include <stdio.h>
-void a(char *s) { printf(s, 10, 10, 10, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 10, 10); } // a
+/*
+outside
+*/
+void a(char *s) { printf(s, 10, 10, 10, 10, 10, 10, 10, 9, 34, 37, 37, 92, 34, 10, 9, 10, 10, 10); }
 int main()
 {
 	printf("char *s = %c%s%c;\n", 34, s, 34); a(s);  // inside the function
