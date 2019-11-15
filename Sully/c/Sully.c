28839b91d3fad6d1829f65e562510ee65db0308b
tnicolas <tnicolas@student.42.fr>
2019-11-15 19:19:06 +0100

    terpri

--- Sully/c/Sully.c
+++ Sully/c/Sully.c
@@ -1,9 +1,10 @@
-#define s "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c#include <stdlib.h>%c#define i %c%u%c%c#define WRITE(fd) dprintf(fd, s, 10, 10, 10, 10, 34, atoi(i) - 1, 34, 10, 10, 34, 34, 34, 34, 34, 37, 37, 92, 34, 34, 34, 34, 34, 34, 34, 10, 10)%c#define MAIN int main() { int fd = open(%cSully_%ci%c.c%c, O_CREAT | O_RDWR, 0700); dprintf(fd, %c#define s %cc%%s%cc%cn%c, 34, s, 34); WRITE(fd); close(fd); atoi(i) && system(%cgcc Sully_%ci%c.c -o Sully_%ci%c && ./Sully_%ci); /* jul */ }%cMAIN%c"
+#define s "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$cint i = %5$d;%1$cchar jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i);return jul;}%1$c#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - 1)%1$c#define MAIN int main() { int fd = open(JULPRINTF(%2$cSully_%3$c1$d.c%2$c, i - 1), O_CREAT | O_RDWR, 0700); dprintf(fd, %2$c#define s %3$cc%3$cs%3$cc%4$cn%2$c, 34, s, 34); WRITE(fd); close(fd); i-1 && system(JULPRINTF(%2$cgcc Sully_%3$c1$d.c -o Sully_%3$c1$d && ./Sully_%3$c1$d%2$c, i - 1)); }%1$cMAIN%1$c"
 #include <stdio.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <unistd.h>
-#define i "5"
-#define WRITE(fd) dprintf(fd, s, 10, 10, 10, 10, 34, atoi(i) - 1, 34, 10, 10, 34, 34, 34, 34, 34, 37, 37, 92, 34, 34, 34, 34, 34, 34, 34, 10, 10)
-#define MAIN int main() { int fd = open("Sully_"i".c", O_CREAT | O_RDWR, 0700); dprintf(fd, "#define s %c%s%c\n", 34, s, 34); WRITE(fd); close(fd); atoi(i) && system("gcc Sully_"i".c -o Sully_"i" && ./Sully_"i); /* jul */ }
+#include <stdlib.h>
+int i = 5;
+char jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i);return jul;}
+#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - 1)
+#define MAIN int main() { int fd = open(JULPRINTF("Sully_%1$d.c", i - 1), O_CREAT | O_RDWR, 0700); dprintf(fd, "#define s %c%s%c\n", 34, s, 34); WRITE(fd); close(fd); i-1 && system(JULPRINTF("gcc Sully_%1$d.c -o Sully_%1$d && ./Sully_%1$d", i - 1)); }
 MAIN
