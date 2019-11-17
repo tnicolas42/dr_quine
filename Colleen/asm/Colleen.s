4d0e0a09842f192b61187b00aa99305bdfd6eb8e
Joffrey Janiec <jjaniec@student.42.fr>
2019-11-17 14:08:09 +0100

    Sully c: no decrement for 1st file

--- Colleen/asm/Colleen.s
+++ Colleen/asm/Colleen.s
@@ -1,21 +1,21 @@
 ;  comment
 section .data
 s:
-.str db ";  comment%2$csection .data%2$cs:%2$c.str db %3$c%1$s%3$c, 0%2$csection .text%2$cglobal _main%2$cextern _printf%2$c_main:  ; comment%2$cpush rbp  ; set up stack frame%2$cmov rbp, rsp%2$clea rdi, [rel s.str]  ; arg 1%2$clea rsi, [rel s.str]  ; arg 2%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$ccall usefull%2$cpop rbp  ; restore stack%2$cmov rax, 0  ; return value%2$cret%2$cusefull:%2$cret%2$c", 0
+.str db ";  comment%2$csection .data%2$cs:%2$c.str db %3$c%1$s%3$c, 0%2$csection .text%2$cglobal _main%2$cextern _printf%2$c_main:%2$cpush rbp  ; set up stack frame%2$cmov rbp, rsp%2$clea rdi, [rel s.str]%2$clea rsi, [rel s.str]%2$cmov rdx, 10%2$cmov rcx, 34%2$ccall _printf%2$ccall usefull%2$cpop rbp%2$cmov rax, 0%2$cret%2$cusefull:%2$cret%2$c", 0
 section .text
 global _main
 extern _printf
-_main:  ; comment
+_main:
 push rbp  ; set up stack frame
 mov rbp, rsp
-lea rdi, [rel s.str]  ; arg 1
-lea rsi, [rel s.str]  ; arg 2
+lea rdi, [rel s.str]
+lea rsi, [rel s.str]
 mov rdx, 10
 mov rcx, 34
 call _printf
 call usefull
-pop rbp  ; restore stack
-mov rax, 0  ; return value
+pop rbp
+mov rax, 0
 ret
 usefull:
 ret
--- Sully/c/Sully.c
+++ Sully/c/Sully.c
@@ -1,10 +1,13 @@
-#define s "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$cint i = %5$d;%1$cchar jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i);return jul;}%1$c#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - 1)%1$c#define MAIN int main() { if (i != -1) { int fd = open(JULPRINTF(%2$cSully_%3$c1$d.c%2$c, i), O_CREAT | O_RDWR, 0700); dprintf(fd, %2$c#define s %3$cc%3$cs%3$cc%4$cn%2$c, 34, s, 34); WRITE(fd); close(fd); system(JULPRINTF(%2$cgcc Sully_%3$c1$d.c -o Sully_%3$c1$d && ./Sully_%3$c1$d%2$c, i)); }}%1$cMAIN%1$c"
+char *s = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$cint i = %5$d;%1$c#ifndef DECREMENT%1$c#define DECREMENT 1%1$c#endif%1$cchar jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i - DECREMENT);return jul;}%1$c#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - DECREMENT)%1$c#define MAIN int main() { if (i != 0) { int fd = open(JULPRINTF(%2$cSully_%3$c1$d.c%2$c, i), O_CREAT | O_RDWR, 0700); dprintf(fd, %2$cchar *s = %3$cc%3$cs%3$cc;%4$cn%2$c, 34, s, 34); WRITE(fd); close(fd); system(JULPRINTF(%2$cgcc Sully_%3$c1$d.c -o Sully_%3$c1$d && ./Sully_%3$c1$d%2$c, i)); }}%1$cMAIN%1$c";
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 int i = 5;
-char jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i);return jul;}
-#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - 1)
-#define MAIN int main() { if (i != -1) { int fd = open(JULPRINTF("Sully_%1$d.c", i), O_CREAT | O_RDWR, 0700); dprintf(fd, "#define s %c%s%c\n", 34, s, 34); WRITE(fd); close(fd); system(JULPRINTF("gcc Sully_%1$d.c -o Sully_%1$d && ./Sully_%1$d", i)); }}
+#ifndef DECREMENT
+#define DECREMENT 1
+#endif
+char jul[100]; char *JULPRINTF(char *s2,int i) {sprintf(jul,s2,i - DECREMENT);return jul;}
+#define WRITE(fd) dprintf(fd, s, 10, 34, 37, 92, i - DECREMENT)
+#define MAIN int main() { if (i != 0) { int fd = open(JULPRINTF("Sully_%1$d.c", i), O_CREAT | O_RDWR, 0700); dprintf(fd, "char *s = %c%s%c;\n", 34, s, 34); WRITE(fd); close(fd); system(JULPRINTF("gcc Sully_%1$d.c -o Sully_%1$d && ./Sully_%1$d", i)); }}
 MAIN
